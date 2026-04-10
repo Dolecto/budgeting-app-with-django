2d113d726f11ab180a3f8d2260b3b900594e203c
Dolecto <115491148+Dolecto@users.noreply.github.com>
2026-04-10 03:34:17 +0200

    feat: update main with reconciliation

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,13 @@
 import os
 from functools import partial
-from ocr_pipeline.ocr_preprocessing import OCRPreprocessingPipeline as Pipeline
-from ocr_pipeline.ocr_text_extraction import load_ocr, extract_text
+from ocr_pipeline.preprocessing import OCRPreprocessingPipeline as Pipeline
+from ocr_pipeline.text_extraction import load_ocr, extract_text
+from ocr_pipeline.text_reconciliation import parse_receipt
 
 
 # TODO: Connect to API.
-input_path = "../testing-materials/receipt_eng_04.jpg"
+input_path = "../testing-materials/receipt_eng_02.jpg"
+receipt_type = "grocery"  # or "restaurant", so far
 img_basename = os.path.splitext(os.path.basename(input_path))[0]
 
 
@@ -21,7 +23,6 @@ pipeline_1 = Pipeline(
 
 pipeline_2 = Pipeline(
     steps=[
-        #Pipeline.normalize,
             partial(Pipeline.denoise, method="bilateral"),
             partial(Pipeline.enhance, method="clahe"),
             partial(Pipeline.upscale, scale=1.15)
@@ -30,16 +31,13 @@ pipeline_2 = Pipeline(
 
 pipeline_3 = Pipeline(
     steps=[
-        #Pipeline.normalize,
         partial(Pipeline.denoise, method="gaussian"),
         partial(Pipeline.enhance, method="clahe"),
-        # partial(Pipeline.binarize, method="otsu"),
     ], name="pipeline_3")
 
 
 pipeline_4 = Pipeline(
     steps=[
-        #Pipeline.normalize,
         partial(Pipeline.denoise, method="nlm"),
         partial(Pipeline.enhance, method="clahe"),
         partial(Pipeline.upscale, scale=1.15),
@@ -51,13 +49,12 @@ pipeline_5 = Pipeline(
         partial(Pipeline.upscale, scale=1.15),
         partial(Pipeline.denoise, method="nlm"),
         partial(Pipeline.enhance, method="clahe"),
-        # partial(Pipeline.denoise, method="bilateral"),
         partial(Pipeline.sharpen, method="unsharp", strength=0.5),
     ], name="pipeline_5")
 
 
 ocr_model = load_ocr()
-
+ocr_outputs = []
 
 for pipeline in [
     pipeline_1, 
@@ -66,11 +63,16 @@ for pipeline in [
     pipeline_4, 
     pipeline_5, 
     ]:
-    extract_text(
-        ocr_model=ocr_model,
-        img=pipeline.run(input_path), 
-        json_output_dir="json_outputs", 
-        filename=f"{img_basename}_{pipeline.name}",
-        debug=True, 
-        debug_dir="ocr_debugging"
+    ocr_outputs.append(
+        extract_text(
+            ocr_model=ocr_model,
+            img=pipeline.run(input_path), 
+            json_output_dir="json_outputs", 
+            filename=f"{img_basename}_{pipeline.name}",
+            debug=True, 
+            debug_dir="ocr_debugging"
+        )
     )
+
+
+result = parse_receipt(ocr_outputs, receipt_type=receipt_type)
